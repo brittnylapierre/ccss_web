5ec3a88f22f771fb56d2f5c4f72c2626ae761793
Brittny Lapierre <brittny.lapierre@gmail.com>
2016-08-13 20:56:59 -0400

    Updated custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
   //$( 'html' ).height($(document).height());
   //console.log('Height: ' + $( 'html' ).height());
 });
+
+$( '.menu li' ).mouseover(function(event){
+	var submenus = $( '.sub-menu' )
+});
