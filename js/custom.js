73b884f0429418be03b5e8ffaeafbb9f706ff6ea
Brittny Lapierre <brittny.lapierre@gmail.com>
2016-08-13 21:14:08 -0400

    Updated custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,26 +3,3 @@ $(document).ready(function(){
   //console.log('Height: ' + $( 'html' ).height());
 });
 
-$( '.menu li' ).mouseenter(function(event){
-  console.log('mouse enter!');
-  var submenus = $( '.menu li .sub-menu' );
-  for(var i = 0; i < submenus.length; i++){
-    submenus[i].style.display = 'inherit';
-    if(submenus[i].parentNode != event.target){
-      submenus[i].style.visibility = 'hidden';
-    }
-    else{
-      submenus[i].style.visibility = 'visible';
-    }
-  }
-});
-
-$( '.menu li' ).mouseleave(function(event){
-  console.log('mouse out!');
-  var submenus = $( '.menu li .sub-menu' );
-  for(var i = 0; i < submenus.length; i++){
-    submenus[i].style.display = 'none';
-    submenus[i].style.visibility = 'visible';
-  }
-});
-
